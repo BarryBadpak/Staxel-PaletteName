8c672cc4a837a3a28b3e17fbce99f42a3b3c0456
Stefan Haneveer <s.haneveer93@gmail.com>
2018-02-17 21:13:31 +0100

    Rewrite to support Sunbeam update and update readme

---
+++ PaletteName/PaletteNameManager.cs
@@ -0,0 +1,34 @@
+﻿using Staxel.Browser;
+using Sunbeam;
+
+namespace PaletteName
+{
+	public class PaletteNameManager : SunbeamMod
+    {
+        public override string ModIdentifier => "PaletteName";
+
+        private string HTMLAsset { get; set; }
+        private string JSAsset { get; set; }
+        private string CSSAsset { get; set; }
+
+        /// <summary>
+        /// Construct a new PaletteNameManager
+        /// </summary>
+        public PaletteNameManager()
+        {
+            this.HTMLAsset = this.AssetLoader.ReadFileContent("Assets/index.min.html");
+            this.JSAsset = this.AssetLoader.ReadFileContent("Assets/main.min.js");
+            this.CSSAsset = this.AssetLoader.ReadFileContent("Assets/style.min.css");
+        }
+
+		/// <summary>
+		/// Inject the UI contents
+		/// </summary>
+		public override void IngameOverlayUILoaded(BrowserRenderSurface surface)
+		{
+			surface.CallPreparedFunction("(() => { const el = document.createElement('style'); el.type = 'text/css'; el.appendChild(document.createTextNode('" + this.CSSAsset + "')); document.head.appendChild(el); })();");
+			surface.CallPreparedFunction("$('#CharacterCreator:not(:has(#CharacterCreator_Info))').append(\"" + this.HTMLAsset + "\");");
+			surface.CallPreparedFunction(this.JSAsset);
+		}
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-﻿# Staxel - PaletteNameMod
+﻿<h1 align="center">Staxel - Palette name tooltip mod</h1>
+<p align="center">
+  <a href="https://github.com/stefanhaan/Staxel-Sunbeam">
+    <img src="https://img.shields.io/badge/Sunbeam-source-yellow.svg?style=flat" alt="Sunbeam" />
+  </a>
+  <a href="http://steamcommunity.com/sharedfiles/filedetails/?id=1301606285">
+    <img src="https://img.shields.io/badge/Steam-release-blue.svg?style=flat" alt="v1.0.9.1" />
+  </a>
+</p>
+
+<p align="center">
+	<img src="PaletteName/preview.png"/>
+</p>
 
 This mod is derived from Sunbeam and modifies the character creation UI slightly.
 This mod adds a tooltip for the palettes, now when you hover over a palette color
